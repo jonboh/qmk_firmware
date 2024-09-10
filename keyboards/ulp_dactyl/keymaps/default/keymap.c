063bf9e6adf27b4b696e3c0ad7ad0cbcc552013b
jonboh <jon.bosque.hernando@gmail.com>
2024-09-10 11:59:32 +0200

    update liftoff and set debugging behind CONSOLE_ENABLE flag

--- keyboards/ulp_dactyl/keymaps/default/keymap.c
+++ keyboards/ulp_dactyl/keymaps/default/keymap.c
@@ -187,6 +187,9 @@ bool is_mouse_record_user(uint16_t keycode, keyrecord_t* record) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif
     update_tri_layer(NUM, NAV, FUNC);
     // update_tri_layer(SYMB, MOUSL, SYMB_MODL);
     // update_tri_layer(SYMB, MOUSR, SYMB_MODR);
@@ -378,10 +381,12 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
 }
 //#endif /* ifdef COMBO_ENABLE */
 
-// void keyboard_post_init_user(void) {
-//   // Customise these values to desired behaviour
-//   debug_enable=true;
-//   //debug_matrix=true;
-//   debug_keyboard=true;
-//   //debug_mouse=true;
-// }
+#ifdef CONSOLE_ENABLE
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  //debug_matrix=true;
+  debug_keyboard=true;
+  //debug_mouse=true;
+}
+#endif
